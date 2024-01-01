97325cc57b5633e6fe241db43fc9cc8a112dbf11
kali <arizonamarco521@gmail.com>
2024-01-01 13:07:31 +0700

    third commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,11 +39,28 @@
                 <h1>Kerut<span>.</span></h1>
             </a>
             <nav id="navbar" class="navbar">
-                <ul>
-                    <li><a href="#hero">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                </ul>
-            </nav><!-- .navbar -->
+                @if (Route::has('login'))
+                    @auth
+                        <ul>
+                        @else
+                            <ul>
+                                <li>
+                                    <a href="{{ route('login') }}"
+                                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
+                                        in</a>
+                                </li>
+
+                                @if (Route::has('register'))
+                                    <li>
+                                        <a href="{{ route('register') }}"
+                                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                                    </li>
+                                @endif
+                            </ul>
+                        @endauth
+                @endif
+            </nav>
+
 
             <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
             <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
